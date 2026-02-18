8c151ae073fc62610efe8d8977ee412ebd2ccdf6
djjr <djjrjr@gmail.com>
2026-02-18 10:39:42 -0500

    tag updates

--- courses.js
+++ courses.js
@@ -851,6 +851,6 @@ window.COURSES = [
   "term": "",
   "campus": "",
   "notes": "",
-  "keywords": ["attention","social media", "critique"]
+  "keywords": ["attention","social-media", "critique"]
 }
 ]
